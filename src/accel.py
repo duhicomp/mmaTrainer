9f1ae9aa0c3add24e994ec241de512125043a7a4
duhicomp <m.makhzomi@gmail.com>
2025-10-25 16:47:02 -0400

    Updated to class implementation

--- src/accel.py
+++ src/accel.py
@@ -3,13 +3,24 @@ from mpu6050 import mpu6050
 from time import sleep
 import math
 # Initialize the MPU-6050 sensor with the I2C address 0x68.
-sensor = mpu6050(0x68)
+
 
 class Accelerator:
-    def __init__():
-        self prev_filtered_x = 0
-        self prev_filtered_y = 0
-        self prev_filtered_z = 0
+    def __init__(self):
+        self.sensor = mpu6050(0x68)
+        self.prev_filtered_x = 0
+        self.prev_filtered_y = 0
+        self.prev_filtered_z = 0
+        
+        self.accel_x = 0
+        self.accel_y = 0
+        self.accel_z = 0
+
+        self.gyro_x = 0
+        self.gyro_y = 0
+        self.gyro_z = 0
+
+        self.temperature = 0
 
     def get_net_acceleration(self, accel_data):
 
@@ -47,19 +58,38 @@ class Accelerator:
 
     print("\nFiltered data:", filtered_data)
     def get_gyro_data(self):
-        gyro_data = sensor.get_gyro_data()
-        return gyro_data
+        gyro_data = self.sensor.get_gyro_data()
+        self.gyro_x = gyro_data['x']
+        self.gyro_y = gyro_data['y']
+        self.gyro_z = gyro_data['z']
+
+    def get_temp(self):
+        self.temperature = self.sensor.get_temp()
 
     def get_accelearator_values(self)
-        accel_data = sensor.get_accel_data()
+        accel_data = self.sensor.get_accel_data()
         if self.prev_filtered_x == 0:
             self.prev_filtered_x = accel_data['x']
+            self.accel_x = prev_filtered_x
         else:
-            
+            self.accel_x = self.low_pass_filter(self.prev_filtered_x, accel_data['x'] )
+
         if self.prev_filtered_y == 0:
             self.prev_filtered_y = accel_data['y']
+            self.accel_y = self.prev_filtered_y
+        else:
+            self.accel_y = self.low_pass_filter(self.prev_filtered_y, accel_data['y'] )
+
         if self.prev_filtered_z == 0:
             self.prev_filtered_z = accel_data['z']
+            self.accel_z = self.prev_filtered_z
+        else:
+            self.accel_z = self.low_pass_filter(self.prev_filtered_z, accel_data['z'] )
+    
+    def get_sensor_information(self):
+        self.get_accelearator_values()
+        self.get_gyro_data()
+        self.get_temp
 
 if __name__ == "__main__":
 # Infinite loop to continuously read data from the sensor.
@@ -68,26 +98,30 @@ if __name__ == "__main__":
 accelerator = Accelerator()
     while True:
         # Retrieve accelerometer data from the sensor.
-        accel_data = sensor.get_accel_data()
+        # accel_data = sensor.get_accel_data()
+        # accelerator.get_accelearator_values()
         # Retrieve gyroscope data from the sensor.
-        gyro_data = sensor.get_gyro_data()
+        # gyro_data = sensor.get_gyro_data()
+        # accelerator.get_gyro_data()
         # Retrieve temperature data from the sensor.
-        temp = sensor.get_temp()
+        # temp = sensor.get_temp()
+
+        accelerator.get_sensor_information()
 
         # Print accelerometer data.
         print("Accelerometer data")
-        print("x: " + str(accel_data['x']))
-        print("y: " + str(accel_data['y']))
-        print("z: " + str(accel_data['z']))
+        print("x: " + str(accelerator.accel_x))
+        print("y: " + str(accelerator.accel_y))
+        print("z: " + str(accelerator.accel_z))
         print("net acceleration: %f"%get_net_acceleration(accel_data))
         # Print gyroscope data.
         print("Gyroscope data")
-        print("x: " + str(gyro_data['x']))
-        print("y: " + str(gyro_data['y']))
-        print("z: " + str(gyro_data['z']))
+        print("x: " + str(accelerator.gyro_x))
+        print("y: " + str(accelerator.gyro_y))
+        print("z: " + str(accelerator.gyro_z))
 
         # Print the temperature in Celsius.
-        print("Temp: " + str(temp) + " C")
+        print("Temp: " + str(accelerator.temperature) + " C")
 
         # Pause for 0.5 seconds before the next read cycle.
         sleep(0.5)
